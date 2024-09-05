187404f0aec55bb7471da6afa695f9b0e4520118
caveDog69 <java_imhoff@yahoo.com>
2024-09-04 21:15:20 -0400

    fxing links

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/reorg-challenge-java-imhoff/'
+  base: '/reorg-challenge-java-imhoff/',
 });
